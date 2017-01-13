40cbc6b347ae7eb1a7ef21b5c8fefa53f9f54d1b
Luis Mauri <lmaurimtfbwy@gmail.com>
2017-01-13 11:55:33 -0500

    Secret key added for production env

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,7 @@ module Cubamodela
 
     config.i18n.default_locale = :en
     config.i18n.available_locales = [:en, :es]
+
+    config.secret_key_base = 'bff083a75590c82f5a2944dde448aa6df73a3220b8c71ec9c5520759d2eb0e8ccc9b6252b0652314d43013a7b40e9ddf8dba94deb0c8f61b9ed9eb058b908df0'
   end
 end
