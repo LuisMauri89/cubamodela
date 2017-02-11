85f867fa07bdc7cc487c20a35bbd92114268270e
Luis Mauri <lmaurimtfbwy@gmail.com>
2017-02-11 18:48:29 -0500

    Assets host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,11 +29,11 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = ENV['ASSET_HOST']
+  config.action_controller.asset_host = "assets.cubamodela.com" # ENV['ASSET_HOST']
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
