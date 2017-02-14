62edd7794c0e7b42410a42bfd713f26d711b15b1
Luis Mauri <lmaurimtfbwy@gmail.com>
2017-02-14 11:13:53 -0500

    Coupon sent email fixing

--- app/jobs/coupon_sent_job.rb
+++ app/jobs/coupon_sent_job.rb
@@ -2,6 +2,7 @@ class CouponSentJob < ApplicationJob
   queue_as :default
 
   def perform(profile, coupon)
+  	# Send coupon to user
     Notification.notify_coupon_sent(profile, coupon)
     CouponMailer.email_coupon_sent(profile, coupon).deliver_now
   end
